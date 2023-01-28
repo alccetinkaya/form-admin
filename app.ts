7eaabbd3b9f103846a2e20d3629dd2c50217b0e2
Ali Cuneyt CETINKAYA <alccetinkaya>
2023-01-28 17:13:42 +0300

    Created web server and added post /user/create method

--- app.ts
+++ app.ts
@@ -1,1 +1,33 @@
-console.log("Hello, world");
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+import { DatabaseService } from './services/database.service';
+import { UserService } from './services/user.service';
+
+const PORT = 3000;
+
+const userSvc = new UserService();
+const dbSvc = new DatabaseService();
+
+// run the server!
+app.listen({ port: PORT }, (err) => {
+    if (err) throw err
+    console.log(`Server is now listening on ${PORT}`);
+})
+
+app.post('/user/create', async (req, res) => {
+    let userData = userSvc.getUserData(req.body);
+    if (!userData) {
+        res.status(401).send("Create user data is invalid!");
+        return;
+    }
+
+    let rval = await dbSvc.createUser(userData);
+    if (!rval) {
+        res.status(404).send("User couldn't be created!");
+        return;
+    }
+
+    res.send("User has successfully created");
+})
