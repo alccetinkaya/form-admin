73befa98a88cf9b45308af8719f5c7d78961018e
Ali Cuneyt CETINKAYA <alccetinkaya>
2023-01-30 15:51:31 +0300

    Fixed user service due to make it testable

--- services/user.service.ts
+++ services/user.service.ts
@@ -1,6 +1,7 @@
 import { UserData, UserLoginData, UserRoleID } from "../models/user.data.model";
+import { DatabaseSvcInterface } from "./database.service";
 
-const UserDataKeys = {
+export const UserDataKeys = {
     FIRST_NAME: "first_name",
     LAST_NAME: "last_name",
     EMAIL: "email",
@@ -8,44 +9,67 @@ const UserDataKeys = {
     ROLE: "role"
 }
 
+export interface UserSvcResp {
+    status: boolean;
+    statusCode: number;
+    message: string;
+}
+
+interface ValidationResp {
+    status: boolean;
+    message: string;
+}
+
 export interface UserSvcInterface {
-    getUserData(data: any): UserData;
-    getUserLoginData(data: any): UserLoginData;
+    create(data: any): Promise<UserSvcResp>;
+    login(data: any): Promise<UserSvcResp>;
+    delete(data: any): Promise<UserSvcResp>;
 }
 
 export class UserService implements UserSvcInterface {
-    validateUserData(data: any): boolean {
+    _dbSvc: DatabaseSvcInterface;
+
+    constructor(dbSvc: DatabaseSvcInterface) {
+        this._dbSvc = dbSvc;
+    }
+
+    validateUserData(data: any): ValidationResp {
         // check if is there any missing key in create user data
         for (const key of Object.values(UserDataKeys)) {
             if (data[key] === null || data[key] === undefined) {
-                console.log(`User data object doesn't have '${key}' key!`);
-                return false;
+                let msg = `User data object doesn't have '${key}' key!`;
+                console.log(msg);
+                return { status: false, message: msg };
             }
         }
 
         // check first name type
         if (typeof data[UserDataKeys.FIRST_NAME] !== 'string') {
-            console.log(`User data first name isn't string!`);
-            return false;
+            let msg = `User data first name isn't string!`;
+            console.log(msg);
+            return { status: false, message: msg };
         }
 
         // check last name type
         if (typeof data[UserDataKeys.LAST_NAME] !== 'string') {
-            console.log(`User data last name isn't string!`);
-            return false;
+            let msg = `User data last name isn't string!`;
+            console.log(msg);
+            return { status: false, message: msg };
         }
 
         // check email type
         if (typeof data[UserDataKeys.EMAIL] !== 'string') {
-            console.log(`User data email isn't string!`);
-            return false;
+            let msg = `User data email isn't string!`;
+            console.log(msg);
+            return { status: false, message: msg };
         }
 
         // check password type
         const userPswd = data[UserDataKeys.PASSWORD];
         if (typeof userPswd !== 'string' && typeof userPswd !== 'number') {
-            console.log(`User data password isn't string or number!`);
-            return false;
+            let msg = `User data password isn't string or number!`;
+            console.log(msg);
+            return { status: false, message: msg };
         }
         if (typeof userPswd === 'number') {
             data[UserDataKeys.PASSWORD] = userPswd.toString();
@@ -54,8 +78,9 @@ export class UserService implements UserSvcInterface {
         // check role type
         const userRole = data[UserDataKeys.ROLE];
         if (typeof userRole !== 'string') {
-            console.log(`User data role isn't string!`);
-            return false;
+            let msg = `User data role isn't string!`;
+            console.log(msg);
+            return { status: false, message: msg };
         }
         // check if role is valid
         let validRole = false;
@@ -69,46 +94,66 @@ export class UserService implements UserSvcInterface {
             validRole = false;
         }
         if (validRole == false) {
-            console.log(`User data role is invalid!`);
-            return false;
+            let msg = `User data role is invalid!`;
+            console.log(msg);
+            return { status: false, message: msg };
         }
 
-        return true;
+        return { status: true, message: null };
     }
 
-    validateUserLoginData(data: any) {
+    validateUserLoginData(data: any): ValidationResp {
         const keys = [UserDataKeys.EMAIL, UserDataKeys.PASSWORD];
         for (const key of keys) {
             if (data[key] === null || data[key] === undefined) {
-                console.log(`User login data object doesn't have '${key}' key!`);
-                return false;
+                let msg = `User login data object doesn't have '${key}' key!`;
+                console.log(msg);
+                return { status: false, message: msg };
             }
         }
 
         // check email type
         if (typeof data[UserDataKeys.EMAIL] !== 'string') {
-            console.log(`User login data email isn't string!`);
-            return false;
+            let msg = `User login data email isn't string!`;
+            console.log(msg);
+            return { status: false, message: msg };
         }
 
         // check password type
         const userPswd = data[UserDataKeys.PASSWORD];
         if (typeof userPswd !== 'string' && typeof userPswd !== 'number') {
-            console.log(`User data password isn't string or number!`);
-            return false;
+            let msg = `User data password isn't string or number!`;
+            console.log(msg);
+            return { status: false, message: msg };
         }
         if (typeof userPswd === 'number') {
             data[UserDataKeys.PASSWORD] = userPswd.toString();
         }
 
-        return true;
+        return { status: true, message: null };
+    }
+
+    validateUserDeleteData(data: any): ValidationResp {
+        const keys = [UserDataKeys.EMAIL];
+        for (const key of keys) {
+            if (data[key] === null || data[key] === undefined) {
+                let msg = `User login data object doesn't have '${key}' key!`;
+                console.log(msg);
+                return { status: false, message: msg };
+            }
+        }
+
+        // check email type
+        if (typeof data[UserDataKeys.EMAIL] !== 'string') {
+            let msg = `User login data email isn't string!`;
+            console.log(msg);
+            return { status: false, message: msg };
+        }
+
+        return { status: true, message: null };
     }
 
     getUserData(data: any): UserData {
-        if (!this.validateUserData(data)) {
-            return null;
-        }
-
         return {
             firstName: data[UserDataKeys.FIRST_NAME],
             lastName: data[UserDataKeys.LAST_NAME],
@@ -119,13 +164,51 @@ export class UserService implements UserSvcInterface {
     }
 
     getUserLoginData(data: any): UserLoginData {
-        if (!this.validateUserLoginData(data)) {
-            return null;
-        }
-
         return {
             email: data[UserDataKeys.EMAIL],
             password: data[UserDataKeys.PASSWORD],
         };
     }
+
+    async create(data: any): Promise<UserSvcResp> {
+        let rval = this.validateUserData(data);
+        if (!rval.status) {
+            return { status: false, statusCode: 403, message: rval.message };
+        }
+
+        let userData = this.getUserData(data);
+
+        let isMailExists = await this._dbSvc.isUserEmailExists(userData.email);
+        if (isMailExists) {
+            return { status: false, statusCode: 403, message: "User email is exists!" };
+        }
+
+        let success = await this._dbSvc.createUser(userData);
+        return success ? { status: true, statusCode: 200, message: "User has successfully created" } : { status: false, statusCode: 500, message: "Couldn't created user" };
+    };
+
+    async login(data: any): Promise<UserSvcResp> {
+        let rval = this.validateUserLoginData(data);
+        if (!rval.status) {
+            return {
+                status: false,
+                statusCode: 403,
+                message: rval.message
+            };
+        }
+
+        let userLoginData = this.getUserLoginData(data);
+        let success = await this._dbSvc.loginUser(userLoginData);
+        return success ? { status: true, statusCode: 200, message: "User has successfully logged in" } : { status: false, statusCode: 500, message: "Couldn't logged in user" };
+    };
+
+    async delete(data: any): Promise<UserSvcResp> {
+        let rval = this.validateUserDeleteData(data);
+        if (!rval.status) {
+            return { status: false, statusCode: 403, message: rval.message };
+        }
+
+        let success = await this._dbSvc.deleteUser(data[UserDataKeys.EMAIL]);
+        return success ? { status: true, statusCode: 200, message: "User has successfully deleted" } : { status: false, statusCode: 500, message: "Couldn't deleted user" };
+    };
 }
