bbaca023d3e7cee13dc52f47a6acfbdb484c37cb
Ali Cuneyt CETINKAYA <alccetinkaya>
2023-01-30 15:51:02 +0300

    Fixed form service due to change of user service

--- services/form.service.ts
+++ services/form.service.ts
@@ -5,12 +5,13 @@ import { AuthID } from '../models/role.data.model';
 import { DatabaseSvcInterface } from './database.service';
 import { UserSvcInterface } from './user.service';
 
-const token =  process.env.TOKEN;
+const token = process.env.TOKEN;
 const config = {
     headers: { Authorization: `Bearer ${token}` }
 };
 
 export interface FormSvcResp {
+    status: boolean;
     statusCode: number;
     message: string;
 }
@@ -69,109 +70,89 @@ export class FormService implements FormSvcInterface {
 
     async validateCreateFormRequest(formReq: FormRequest): Promise<FormSvcResp> {
         // check if user credential is valid
-        let userLogin = this._userSvc.getUserLoginData(formReq.credential);
-        if (!userLogin) {
-            console.log("User login data is invalid!");
-            return { statusCode: 404, message: "User login data is invalid!" };
-        }
-        if (!await this._dbSvc.loginUser(userLogin)) {
-            console.log("User credential is invalid!");
-            return { statusCode: 404, message: "User credential is invalid!" };
+        let resp = await this._userSvc.login(formReq.credential);
+        if (!resp.status) {
+            return { status: false, statusCode: resp.statusCode, message: resp.message };
         }
 
         // check if user has authority to create form
-        let userRole = await this._dbSvc.getUserRole(userLogin);
+        let userRole = await this._dbSvc.getUserRole(formReq.credential);
         let auth = await this._dbSvc.userHasAuth(userRole, AuthID.CREATE_FORM);
         if (!auth) {
             console.log("User doesn't have authority to create form!");
-            return { statusCode: 404, message: "User doesn't have authority to create form!" };
+            return { status: false, statusCode: 404, message: "User doesn't have authority to create form!" };
         }
 
         // check if form label is exists
         let formLabel = formReq.label;
         if (await this._dbSvc.isFormExists(formLabel)) {
             console.log("Form label is exists!");
-            return { statusCode: 404, message: "Form label is exists!" };
+            return { status: false, statusCode: 404, message: "Form label is exists!" };
         }
 
-        return { statusCode: 200, message: null };
+        return { status: true, statusCode: 200, message: null };
     }
 
     async validateUpdateFormRequest(formReq: FormRequest): Promise<FormSvcResp> {
         // check if user credential is valid
-        let userLogin = this._userSvc.getUserLoginData(formReq.credential);
-        if (!userLogin) {
-            console.log("User login data is invalid!");
-            return { statusCode: 404, message: "User login data is invalid!" };
-        }
-        if (!await this._dbSvc.loginUser(userLogin)) {
-            console.log("User credential is invalid!");
-            return { statusCode: 404, message: "User credential is invalid!" };
+        let resp = await this._userSvc.login(formReq.credential);
+        if (!resp.status) {
+            return { status: false, statusCode: resp.statusCode, message: resp.message };
         }
 
         // check if user has authority to edit form
-        let userRole = await this._dbSvc.getUserRole(userLogin);
+        let userRole = await this._dbSvc.getUserRole(formReq.credential);
         let auth = await this._dbSvc.userHasAuth(userRole, AuthID.EDIT_FORM);
         if (!auth) {
             console.log("User doesn't have authority to edit form!");
-            return { statusCode: 404, message: "User doesn't have authority to edit form!" };
+            return { status: false, statusCode: 404, message: "User doesn't have authority to edit form!" };
         }
 
         // check question types. it should be a single choise and multiple selection
         for (const obj of formReq.data.fields) {
             if (obj.properties.allow_other_choice) {
                 console.log("It isn't allowed other choices!");
-                return { statusCode: 404, message: "It isn't allowed other choices!" };
+                return { status: false, statusCode: 404, message: "It isn't allowed other choices!" };
             }
         }
 
-        return { statusCode: 200, message: null };
+        return { status: true, statusCode: 200, message: null };
     }
 
     async validateDeleteFormRequest(formReq: FormRequest): Promise<FormSvcResp> {
         // check if user credential is valid
-        let userLogin = this._userSvc.getUserLoginData(formReq.credential);
-        if (!userLogin) {
-            console.log("User login data is invalid!");
-            return { statusCode: 404, message: "User login data is invalid!" };
-        }
-        if (!await this._dbSvc.loginUser(userLogin)) {
-            console.log("User credential is invalid!");
-            return { statusCode: 404, message: "User credential is invalid!" };
+        let resp = await this._userSvc.login(formReq.credential);
+        if (!resp.status) {
+            return { status: false, statusCode: resp.statusCode, message: resp.message };
         }
 
         // check if user has authority to delete form
-        let userRole = await this._dbSvc.getUserRole(userLogin);
+        let userRole = await this._dbSvc.getUserRole(formReq.credential);
         let auth = await this._dbSvc.userHasAuth(userRole, AuthID.DELETE_FORM);
         if (!auth) {
             console.log("User doesn't have authority to delete form!");
-            return { statusCode: 404, message: "User doesn't have authority to delete form!" };
+            return { status: false, statusCode: 404, message: "User doesn't have authority to delete form!" };
         }
 
-        return { statusCode: 200, message: null };
+        return { status: true, statusCode: 200, message: null };
     }
 
     async validateViewFormRequest(formReq: FormRequest): Promise<FormSvcResp> {
         // check if user credential is valid
-        let userLogin = this._userSvc.getUserLoginData(formReq.credential);
-        if (!userLogin) {
-            console.log("User login data is invalid!");
-            return { statusCode: 404, message: "User login data is invalid!" };
-        }
-        if (!await this._dbSvc.loginUser(userLogin)) {
-            console.log("User credential is invalid!");
-            return { statusCode: 404, message: "User credential is invalid!" };
+        let resp = await this._userSvc.login(formReq.credential);
+        if (!resp.status) {
+            return { status: false, statusCode: resp.statusCode, message: resp.message };
         }
 
         // check if user has authority to view form
-        let userRole = await this._dbSvc.getUserRole(userLogin);
+        let userRole = await this._dbSvc.getUserRole(formReq.credential);
         let auth = await this._dbSvc.userHasAuth(userRole, AuthID.VIEW_FORM);
         if (!auth) {
             console.log("User doesn't have authority to view form!");
-            return { statusCode: 404, message: "User doesn't have authority to view form!" };
+            return { status: false, statusCode: 404, message: "User doesn't have authority to view form!" };
         }
 
-        return { statusCode: 200, message: null };
+        return { status: true, statusCode: 200, message: null };
     }
 
     async createForm(data: any): Promise<FormSvcResp> {
@@ -179,12 +160,12 @@ export class FormService implements FormSvcInterface {
             // get form request data
             let formReq = this.getFormRequest(data);
             if (!formReq) {
-                return { statusCode: 404, message: "Form request object is invalid!" };
+                return { status: false, statusCode: 404, message: "Form request object is invalid!" };
             }
 
             // validate request
             let resp = await this.validateCreateFormRequest(data);
-            if (resp.statusCode !== 200) {
+            if (!resp.status) {
                 return resp;
             }
 
@@ -193,13 +174,13 @@ export class FormService implements FormSvcInterface {
             const statusCode = response.status;
             if (statusCode >= 200 && statusCode <= 300) {
                 if (!await this._dbSvc.addForm(formReq.label, response.data.id)) {
-                    return { statusCode: 500, message: "Form couldn't created in database" };
+                    return { status: false, statusCode: 500, message: "Form couldn't created in database" };
                 }
             }
-            return { statusCode: response.status, message: "Form has successfully created" };
+            return { status: true, statusCode: response.status, message: "Form has successfully created" };
         } catch (error) {
             console.log(`Couldn't created form: ${error}`);
-            return { statusCode: 500, message: `Couldn't created form: ${error}` };
+            return { status: false, statusCode: 500, message: `Couldn't created form: ${error}` };
         }
     }
 
@@ -208,12 +189,12 @@ export class FormService implements FormSvcInterface {
             // get form request data
             let formReq = this.getFormRequest(data);
             if (!formReq) {
-                return { statusCode: 404, message: "Form request object is invalid!" };
+                return { status: false, statusCode: 404, message: "Form request object is invalid!" };
             }
 
             // validate request
             let resp = await this.validateUpdateFormRequest(formReq);
-            if (resp.statusCode !== 200) {
+            if (!resp.status) {
                 return resp;
             }
 
@@ -221,15 +202,15 @@ export class FormService implements FormSvcInterface {
             let formData = await this._dbSvc.getForm(formReq.label);
             if (!formData) {
                 console.log("Couldn't found form label!");
-                return { statusCode: 404, message: "Couldn't found form label!" };
+                return { status: false, statusCode: 404, message: "Couldn't found form label!" };
             }
 
             // send update form request
             const response = await axios.put(process.env.FORM_URL + "/forms/" + formData.formId, formReq.data, config);
-            return { statusCode: response.status, message: "Form has successfully updated" };
+            return { status: true, statusCode: response.status, message: "Form has successfully updated" };
         } catch (error) {
             console.log(`Couldn't updated form: ${error}`);
-            return { statusCode: 500, message: `Couldn't updated form: ${error}` };
+            return { status: false, statusCode: 500, message: `Couldn't updated form: ${error}` };
         }
     }
 
@@ -238,12 +219,12 @@ export class FormService implements FormSvcInterface {
             // get form request data
             let formReq = this.getFormRequest(data);
             if (!formReq) {
-                return { statusCode: 404, message: "Form request object is invalid!" };
+                return { status: false, statusCode: 404, message: "Form request object is invalid!" };
             }
 
             // validate request
             let resp = await this.validateDeleteFormRequest(formReq);
-            if (resp.statusCode !== 200) {
+            if (!resp.status) {
                 return resp;
             }
 
@@ -251,20 +232,20 @@ export class FormService implements FormSvcInterface {
             let formData = await this._dbSvc.getForm(formReq.label);
             if (!formData) {
                 console.log("Couldn't found form label!");
-                return { statusCode: 404, message: "Couldn't found form label!" };
+                return { status: false, statusCode: 404, message: "Couldn't found form label!" };
             }
 
             // send delete form request
             const response = await axios.delete(process.env.FORM_URL + "/forms/" + formData.formId, config);
             if (response.status === 204) {
                 if (!await this._dbSvc.deleteForm(formData.label)) {
-                    return { statusCode: 500, message: "Form couldn't deleted from database" };
+                    return { status: false, statusCode: 500, message: "Form couldn't deleted from database" };
                 }
             }
-            return { statusCode: response.status, message: "Form has successfully deleted" };
+            return { status: true, statusCode: response.status, message: "Form has successfully deleted" };
         } catch (error) {
             console.log(`Couldn't deleted form: ${error}`);
-            return { statusCode: 500, message: `Couldn't deleted form: ${error}` };
+            return { status: false, statusCode: 500, message: `Couldn't deleted form: ${error}` };
         }
     }
 
@@ -273,12 +254,12 @@ export class FormService implements FormSvcInterface {
             // get form request data
             let formReq = this.getFormRequest(data);
             if (!formReq) {
-                return { statusCode: 404, message: "Form request object is invalid!" };
+                return { status: false, statusCode: 404, message: "Form request object is invalid!" };
             }
 
             // validate request
             let resp = await this.validateViewFormRequest(formReq);
-            if (resp.statusCode !== 200) {
+            if (!resp.status) {
                 return resp;
             }
 
@@ -286,15 +267,15 @@ export class FormService implements FormSvcInterface {
             let formData = await this._dbSvc.getForm(formReq.label);
             if (!formData) {
                 console.log("Couldn't found form label!");
-                return { statusCode: 404, message: "Couldn't found form label!" };
+                return { status: false, statusCode: 404, message: "Couldn't found form label!" };
             }
 
             // send view form request
             const response = await axios.get(process.env.FORM_URL + "/forms/" + formData.formId, config);
-            return { statusCode: response.status, message: response.data };
+            return { status: true, statusCode: response.status, message: response.data };
         } catch (error) {
             console.log(`Couldn't viewed form: ${error}`);
-            return { statusCode: 500, message: `Couldn't viewed form: ${error}` };
+            return { status: false, statusCode: 500, message: `Couldn't viewed form: ${error}` };
         }
     }
 }
